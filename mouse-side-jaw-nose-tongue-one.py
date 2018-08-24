60b602c2feabff6c554cc12f295348320f2ecec9
Adam L. Taylor <adamlyletaylor@gmail.com>
2018-08-24 11:28:12 -0400

    Initial commit

---
+++ mouse-side-jaw-nose-tongue-one.py
@@ -0,0 +1,98 @@
+#! /usr/bin/python3
+
+import sys
+import os
+import tempfile
+import shutil
+import runpy
+import subprocess
+import pwd
+
+def get_username():
+    return pwd.getpwuid( os.getuid() )[ 0 ]
+
+def evaluate_on_one_video(source_input_file_name):
+    this_script_path = os.path.realpath(__file__)
+    this_script_folder_path = os.path.dirname(this_script_path)
+    source_dlc_root_path = os.path.normpath(os.path.join(this_script_folder_path, "dlc"))
+
+    initial_folder_path = os.getcwd()
+    #temp_folder_path = tempfile.mkdtemp()
+    print("username is %s" % get_username()) 
+    temp_folder_path = os.path.join("/scratch", get_username())
+    print("temp_folder_path is %s" % temp_folder_path) 
+
+    did_create_dlc_container_path = False
+    try:
+        dlc_container_path = tempfile.mkdtemp(prefix=temp_folder_path+"/")
+        did_create_dlc_container_path = True
+        print("dlc_container_path is %s" % dlc_container_path) 
+
+        dlc_root_path = os.path.join(dlc_container_path, "dlc") ;
+        print("dlc_root_path is %s" % dlc_root_path) 
+
+        shutil.copytree(source_dlc_root_path, dlc_root_path)
+
+        #source_input_file_name = sys.argv[1]
+        source_input_file_path = os.path.abspath(source_input_file_name)
+
+        source_input_file_base_name = os.path.basename(source_input_file_path)
+        (input_file_stem_name, input_file_extension) = os.path.splitext(source_input_file_base_name)
+
+        input_file_path = os.path.join(dlc_root_path,
+                                       "videos",
+                                       "the-video" + input_file_extension)
+
+        # Copy the input video to the DLC "videos" folder, should be on scratch drive
+        shutil.copyfile(source_input_file_path, input_file_path)
+
+        # Synthesize absolute path to the video-analysis script
+        analysis_folder_path = os.path.join(dlc_root_path, "Analysis-tools")
+        analyze_video_script_path = os.path.join(analysis_folder_path, "AnalyzeVideos.py")
+        print("analysis_folder_path: %s\n" % analysis_folder_path)
+        print("analyze_video_script_path: %s\n" % analyze_video_script_path)
+
+        #  
+        os.chdir(analysis_folder_path)
+        #exec(open(analyze_video_script_path).read())
+        runpy.run_path(analyze_video_script_path)
+        #subprocess.call("bsub -q gpu_any -n1 -gpu "num=1" cd %s ; "  )
+        os.chdir(initial_folder_path)
+
+        output_file_extension = ".h5"
+        source_output_file_path = os.path.join(dlc_root_path,
+                                               "videos",
+                                               "the-video" + "DeepCut_resnet50_licking-sideJul10shuffle1_1000000" + output_file_extension)
+        if len(sys.argv) >= 3 :
+            destination_output_file_name = sys.argv[2]
+            if os.path.isabs(destination_output_file_name) :
+                destination_output_file_path = destination_output_file_name
+            else :
+                destination_output_file_path = os.path.abspath(destination_output_file_name)
+        else :
+            source_input_file_folder_path = os.path.dirname(source_input_file_path)
+            destination_output_file_path = os.path.join(source_input_file_folder_path,
+                                                        input_file_stem_name + output_file_extension)
+
+        print("About to copy result to final location...")
+        print("source_output_file_path: %s" % source_output_file_path)
+        print("destination_output_file_path: %s" % destination_output_file_path)
+        shutil.copyfile(source_output_file_path, destination_output_file_path)
+
+        # Remove the input movie video
+        #os.remove(source_input_file_path)
+
+        # Remove the temporary folder we created
+        shutil.rmtree(dlc_container_path)
+    except Exception as e:
+        if did_create_dlc_container_path :
+            shutil.rmtree(dlc_container_path)
+        os.chdir(initial_folder_path)
+        raise e
+
+
+
+# main
+video_file_path = os.path.abspath(sys.argv[1])
+evaluate_on_one_video(video_file_path)
+
---
+++ mouse-side-jaw-nose-tongue.py
@@ -0,0 +1,104 @@
+#! /usr/bin/python3
+
+import sys
+import os
+import tempfile
+import shutil
+import runpy
+import subprocess
+import pwd
+
+def does_match_extension(file_name, target_extension) :
+    # target_extension should include the dot
+    extension = os.path.splitext(file_name)[1]
+    return (extension == target_extension)
+
+
+def replace_extension(file_name, new_extension) :
+    # new_extension should include the dot
+    base_name = os.path.splitext(file_name)[0]
+    return base_name + new_extension
+
+
+# this is the function we use to recurse
+def evaluate_on_folder(source_folder_path, n_submitted):
+
+    # print to show progress
+    print("%s:" % source_folder_path)
+
+    # get a list of all files and dirs in the source, dest dirs
+    try:
+        source_entries = os.listdir(source_folder_path)
+    except FileNotFoundError :
+        # if we can't list the dir, warn but continue
+        print("Warning: Folder %s doesn't seem to exist" % source_folder_path)   
+        return n_submitted
+    except PermissionError :
+        # if we can't list the dir, warn but continue
+        print("Warning: can't list contents of folder %s due to permissions error" % source_folder_path)
+        return n_submitted
+    
+    # separate source file, dir names
+    names_of_files_in_source_folder = []
+    names_of_folders_in_source_folder = []
+    for source_entry in source_entries :
+        entry_path = os.path.join(source_folder_path, source_entry)
+        if os.path.isdir(entry_path) :
+            names_of_folders_in_source_folder.append(source_entry)
+        elif os.path.isfile(entry_path) :
+            names_of_files_in_source_folder.append(source_entry)
+            
+    # scan the source files, spawn a job for any without a .h5 file, 
+    # or that are newer than the .h5 file
+    for source_file_name in names_of_files_in_source_folder:
+        if does_match_extension(source_file_name, ".avi") :
+            source_file_path = os.path.join(source_folder_path, source_file_name)
+            target_file_path = os.path.join(source_folder_path, replace_extension(source_file_name, ".h5"))
+            if os.path.exists(target_file_path) :
+                if os.path.isdir(target_file_path) :
+                    shutil.rmtree(target_file_path)
+                    do_it = True
+                else :
+                    if os.path.isfile(target_file_path) :
+                        # run the script only if the source is more recent than the target
+                        source_modification_time = os.path.getmtime(source_file_path)
+                        target_modification_time = os.path.getmtime(target_file_path) 
+                        print("source mod time: %s" % source_modification_time)
+                        print("target mod time: %s" % target_modification_time)
+                        do_it = ( source_modification_time >= target_modification_time )
+                    else :
+                        # The file exists, but is neither a file for a folder.  WTF?
+                        # We'll try to proceed
+                        do_it = True                
+            else :
+                # target file does not exist
+                do_it = True
+            if do_it :
+                stdout_file_path = replace_extension(source_file_path, '-stdout.txt')
+                stderr_file_path = replace_extension(source_file_path, '-stderr.txt')   
+                command_line = 'bsub -o "%s" -e "%s" -q gpu_any -n1 -gpu "num=1" singularity exec -B /scratch,/nrs --nv dlc.simg python3 mouse-side-jaw-nose-tongue-one.py "%s"' % (stdout_file_path, stderr_file_path, source_file_path)
+                print('About to subprocess.call(): %s' % command_line)
+                print("PATH: %s" % os.environ['PATH'])
+                print("PWD: %s" % os.environ['PWD'])
+                subprocess.call(command_line, shell=True)
+                n_submitted = n_submitted + 1
+        
+    # for each dir in names_of_folders_in_source_folder, recurse
+    for source_subfolder_name in names_of_folders_in_source_folder:
+        n_submitted = evaluate_on_folder(os.path.join(source_folder_path, source_subfolder_name),
+                                      n_submitted)
+                    
+    # return the updated count of copied files
+    return n_submitted
+
+# end of evaluate_on_folder
+
+
+
+
+# main
+source_folder_path = os.path.abspath(sys.argv[1])
+n_submitted = evaluate_on_folder(source_folder_path, 0)
+
+# print the number of jobs submitted
+print("%d jobs submitted" % n_submitted)
