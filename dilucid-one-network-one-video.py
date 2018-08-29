68b8d95b76850feaf6654ac8972117f698a92781
Adam L. Taylor <taylora@janelia.hhmi.org>
2018-08-28 21:10:14 -0400

    First draft almost done

--- dilucid-one-network-one-video.py
+++ dilucid-one-network-one-video.py
@@ -11,100 +11,140 @@ import pwd
 def get_username():
     return pwd.getpwuid( os.getuid() )[ 0 ]
 
-def evaluate_on_one_video(source_input_file_name, lock_file_name, destination_output_file_name):
+def is_empty(list) :
+    return len(list)==0
+
+def evaluate_on_one_video(input_file_path, 
+                          network_folder_path, 
+                          lock_file_path, 
+                          output_file_path):
+
+    # Determine the absolute path to the "reference" DLC folder
     this_script_path = os.path.realpath(__file__)
     this_script_folder_path = os.path.dirname(this_script_path)
-    source_dlc_root_path = os.path.normpath(os.path.join(this_script_folder_path, "dlc-modded"))
+    template_dlc_root_folder_path = os.path.normpath(os.path.join(this_script_folder_path, "dlc-modded"))
 
-    initial_folder_path = os.getcwd()
-    #temp_folder_path = tempfile.mkdtemp()
+    # Determine the absolute path to the parent temp folder that we can write to (e.g. /scratch/svobodalab)
+    initial_working_folder_path = os.getcwd()
     print("username is %s" % get_username()) 
-    temp_folder_path = os.path.join("/scratch", get_username())
-    print("temp_folder_path is %s" % temp_folder_path) 
+    scratch_folder_path = os.path.join("/scratch", get_username())
+    print("scratch_folder_path is %s" % scratch_folder_path) 
 
-    did_create_dlc_container_path = False
+    scratch_dlc_container_path_maybe = []  # want to keep track of this so we know whether or not to delete it
     try:
-        dlc_container_path = tempfile.mkdtemp(prefix=temp_folder_path+"/")
-        did_create_dlc_container_path = True
-        print("dlc_container_path is %s" % dlc_container_path) 
+        # Make a temporary folder to hold the temporary DLC folder
+        # (want to have them for different instances running on same
+        # node without collisions)
+        scratch_dlc_container_path = tempfile.mkdtemp(prefix=scratch_folder_path+"/")
+        scratch_dlc_container_path_maybe = [scratch_dlc_container_path]
+        print("scratch_dlc_container_path is %s" % scratch_dlc_container_path) 
 
-        dlc_root_path = os.path.join(dlc_container_path, "dlc-modded") ;
-        print("dlc_root_path is %s" % dlc_root_path) 
+        # Determine the absolute path to the temporary DLC folder 
+        scratch_dlc_root_folder_path = os.path.join(scratch_dlc_container_path, "dlc-modded") ;
+        print("scratch_dlc_root_folder_path is %s" % scratch_dlc_root_folder_path) 
 
-        shutil.copytree(source_dlc_root_path, dlc_root_path)
+        # Copy the reference DLC folder to the temporary one
+        shutil.copytree(template_dlc_root_folder_path, scratch_dlc_root_folder_path)
 
-        #source_input_file_name = sys.argv[1]
-        source_input_file_path = os.path.abspath(source_input_file_name)
+        # Copy the configuration file into the scratch DLC folder
+        configuration_file_name = "myconfig_analysis.py" ;
+        configuration_file_path = os.path.join(network_folder_path, configuration_file_name)
+        scratch_configuration_file_path = os.path.join(scratch_dlc_root_folder_path, configuration_file_name)
+        shutil.copyfile(configuration_file_path, scratch_configuration_file_path_path)
+        
+        # There should be exactly one folder within the network folder.
+        # That's the "model" folder, which contains two folders: "test" and "train".
+        # get a list of all files and dirs in the source, dest dirs
+        try:
+            network_folder_contents = os.listdir(network_folder_path)
+        except FileNotFoundError :
+            # if we can't list the folder, give up
+            raise RuntimeError("Network folder %s doesn't seem to exist" % network_folder_path)   
+        except PermissionError :
+            # if we can't list the dir, warn but continue
+            raise RuntimeError("Can't list contents of network folder %s due to permissions error" % network_folder_path)
+        names_of_folders = [item
+                            for item
+                            in network_folder_contents
+                            if os.path.isdir(os.path.join(network_folder_path, item))]
+        if is_empty(names_of_folders) :
+            raise RuntimeError("Network folder %s has no subfolders" % network_folder_path)
+        model_folder_name = names_of_folders[0] ;
+        model_folder_path = os.path.join(network_folder_path, model_folder_name)
+        
+        # Copy the model folder to the scratch DLC folder, in the right place
+        scratch_model_folder_path = os.join(scratch_dlc_root_folder_path, "pose-tensorflow", "models", model_folder_name)
+        shutil.copytree(model_folder_path, scratch_model_folder_path)
 
-        source_input_file_base_name = os.path.basename(source_input_file_path)
-        (input_file_stem_name, input_file_extension) = os.path.splitext(source_input_file_base_name)
+        # Copy the input video to the "videos" folder within the scratch DLC folder
+        input_file_base_name = os.path.basename(input_file_path)
+        (input_file_stem_name, input_file_extension) = os.path.splitext(input_file_base_name)
+        scratch_input_file_path = os.path.join(scratch_dlc_root_folder_path,
+                                                 "videos",
+                                                 "the-video" + input_file_extension)
+        shutil.copyfile(input_file_path, scratch_input_file_path)
 
-        input_file_path = os.path.join(dlc_root_path,
-                                       "videos",
-                                       "the-video" + input_file_extension)
-
-        # Copy the input video to the DLC "videos" folder, should be on scratch drive
-        shutil.copyfile(source_input_file_path, input_file_path)
-
-        # Synthesize absolute path to the video-analysis script
-        analysis_folder_path = os.path.join(dlc_root_path, "Analysis-tools")
+        # Determine absolute path to the (scratch version of the) video-analysis script
+        analysis_folder_path = os.path.join(scratch_dlc_root_folder_path, "Analysis-tools")
         analyze_video_script_path = os.path.join(analysis_folder_path, "AnalyzeVideos.py")
         print("analysis_folder_path: %s\n" % analysis_folder_path)
         print("analyze_video_script_path: %s\n" % analyze_video_script_path)
 
-        #  
+        # cd into the scratch analysis folder, run the script, cd back
         os.chdir(analysis_folder_path)
-        #exec(open(analyze_video_script_path).read())
         runpy.run_path(analyze_video_script_path)
-        #subprocess.call("bsub -q gpu_any -n1 -gpu "num=1" cd %s ; "  )
-        os.chdir(initial_folder_path)
+        os.chdir(initial_working_folder_path)
 
-        output_file_extension = ".h5"
-        source_output_file_path = os.path.join(dlc_root_path,
-                                               "videos",
-                                               "the-video" + "DeepCut_resnet50_licking-side2Jul11shuffle1_400000" + output_file_extension)
-        destination_output_file_path = os.path.abspath(destination_output_file_name)
-
-        # Check the umask
+        # Print the umask
         print("umask -S:")
         os.system("umask -S")
         print("")
 
+        # Set the umask so group members can delete stuff
         # The umask is u=rwx, g=rx, u=rx, for some reason...
         # (The bsub?  The singularity container?)  Dunno...
         # Anyway, we change it so that the final .h5 file is group-writable,
         # but others have no access
         os.umask(0o007)
 
+        # Copy the scratch output file to the final output file location
+        output_file_extension = ".h5"
+        scratch_output_file_path = os.path.join(scratch_dlc_root_folder_path,
+                                                "videos",
+                                                "the-video" + 
+                                                    "DeepCut_resnet50_licking-side2Jul11shuffle1_400000" + 
+                                                    output_file_extension)
         print("About to copy result to final location...")
-        print("source_output_file_path: %s" % source_output_file_path)
-        print("destination_output_file_path: %s" % destination_output_file_path)
-        shutil.copyfile(source_output_file_path, destination_output_file_path)
+        print("scratch_output_file_path: %s" % scratch_output_file_path)
+        print("output_file_path: %s" % output_file_path)
+        shutil.copyfile(scratch_output_file_path, output_file_path)
 
-        # Remove the temporary folder we created
-        shutil.rmtree(dlc_container_path)
+        # Remove the scratch folder we created to hold the scratch DLC folder
+        shutil.rmtree(scratch_dlc_container_path)
 
         # Remove the input movie video, if we have adequate permissions
+        # But just keep on going if we don't
         try:
-            os.remove(source_input_file_path)
+            os.remove(input_file_path)
         except Exception as e :
-            print('Tried to delete %s, but was unable to do so for some reason' % source_input_file_path)
+            print('Tried to delete %s, but was unable to do so for some reason' % input_file_path)
 
         # Remove the lock file
-        lock_file_path = os.path.abspath(lock_file_name)
         if os.path.exists(lock_file_path) :
             os.remove(lock_file_path)
 
     except Exception as e:
-        # Remove the temporary folder
-        if did_create_dlc_container_path :
-            shutil.rmtree(dlc_container_path)
+        # Try to clean up some before re-throwing
+
+        # Remove the scratch folder
+        if not is_empty(scratch_dlc_container_path_maybe) :
+            shutil.rmtree(scratch_dlc_container_path_maybe[0])
         # Remove the lock file
-        lock_file_path = os.path.join(source_input_file_name + ".lock")
+        lock_file_path = os.path.join(input_file_name + ".lock")
         if os.path.exists(lock_file_path) :
             os.remove(lock_file_path)
         # cd back to the initial folder
-        os.chdir(initial_folder_path)
+        os.chdir(initial_working_folder_path)
         # Re-throw the exception
         raise e
 
@@ -112,7 +152,8 @@ def evaluate_on_one_video(source_input_file_name, lock_file_name, destination_ou
 
 # main
 video_file_path = os.path.abspath(sys.argv[1])
-lock_file_path = os.path.abspath(sys.argv[2])
-output_file_path = os.path.abspath(sys.argv[3])
-evaluate_on_one_video(video_file_path, lock_file_path, output_file_path)
+network_folder_path = os.path.abspath(sys.argv[2])
+lock_file_path = os.path.abspath(sys.argv[3])
+output_file_path = os.path.abspath(sys.argv[4])
+evaluate_on_one_video(video_file_path, network_folder_path, lock_file_path, output_file_path)
 
--- dilucid.py
+++ dilucid.py
@@ -1,5 +1,9 @@
 #! /usr/bin/python3
 
+# This script is run on the dilucid root folder.  It enumerates all
+# the folders within that folder (not recursively), and then calls
+# dilucid-one-network.py on each of the subfolders.
+
 import sys
 import os
 import tempfile
@@ -14,45 +18,19 @@ def does_match_extension(file_name, target_extension) :
     extension = os.path.splitext(file_name)[1]
     return (extension == target_extension)
 
-
 def replace_extension(file_name, new_extension) :
     # new_extension should include the dot
     base_name = os.path.splitext(file_name)[0]
     return base_name + new_extension
 
+def is_empty(list) :
+    return len(list)==0
 
-# this is the function we use to recurse
-def evaluate_on_folder(source_folder_path, output_folder_path, n_submitted):
-
-    # print to show progress
-    print("%s:" % source_folder_path)
-
-    # get a list of all files and dirs in the source, dest dirs
-    try:
-        source_entries = os.listdir(source_folder_path)
-    except FileNotFoundError :
-        # if we can't list the dir, warn but continue
-        print("Warning: Folder %s doesn't seem to exist" % source_folder_path)   
-        return n_submitted
-    except PermissionError :
-        # if we can't list the dir, warn but continue
-        print("Warning: can't list contents of folder %s due to permissions error" % source_folder_path)
-        return n_submitted
-    
-    # separate source file, dir names
-    names_of_files_in_source_folder = []
-    names_of_folders_in_source_folder = []
-    for source_entry in source_entries :
-        entry_path = os.path.join(source_folder_path, source_entry)
-        if os.path.isdir(entry_path) :
-            names_of_folders_in_source_folder.append(source_entry)
-        elif os.path.isfile(entry_path) :
-            names_of_files_in_source_folder.append(source_entry)
-            
-    # scan the source files, spawn a job for any without a .h5 file, 
-    # or that are newer than the .h5 file
+def process_files_in_one_folder(source_folder_path, names_of_source_files, output_folder_path, network_folder_path, n_submitted)
+    # Scan the source files, spawn a job for any without a .h5 file, 
+    # or that are newer than the .h5 file.
     has_been_run_on_one_file_in_this_folder = False
-    for source_file_name in names_of_files_in_source_folder:
+    for source_file_name in names_of_source_files:
         if does_match_extension(source_file_name, ".avi") :
             source_file_path = os.path.join(source_folder_path, source_file_name)
             lock_file_path = os.path.join(output_folder_path, source_file_name + ".lock")
@@ -78,7 +56,7 @@ def evaluate_on_folder(source_folder_path, output_folder_path, n_submitted):
                                   ( current_time > source_modification_time + waiting_period ) )
                     else :
                         # The file exists, but is neither a file or a folder.  WTF?
-                        print("An object exists at target location %s, but it's neigher a file nor a folder.  Not submitting a job." 
+                        print("An object exists at target location %s, but it's neither a file nor a folder.  Not submitting a job." 
                               % target_file_path)
                         do_it = False
             else :
@@ -99,24 +77,117 @@ def evaluate_on_folder(source_folder_path, output_folder_path, n_submitted):
                     print("lock_file_path: %s"   % lock_file_path)
                     print("target_file_path: %s" % target_file_path)
                     command_line = ( ( 'bsub -o "%s" -e "%s" -q gpu_any -n1 -gpu "num=1" singularity exec ' +
-                                       '-B /scratch,/nrs --nv dlc.simg python3 mouse-side-jaw-nose-tongue-one.py "%s" "%s" "%s"' )
-                                     % (stdout_file_path, stderr_file_path, source_file_path, lock_file_path, target_file_path) )
+                                       '-B /scratch,/nrs --nv dlc.simg python3 dilucid-one-network-one-video.py "%s" "%s" "%s" "%s"' )
+                                     % (stdout_file_path, stderr_file_path, source_file_path, network_file_path, lock_file_path, target_file_path) )
                     print('About to subprocess.call(): %s' % command_line)
                     print("PATH: %s" % os.environ['PATH'])
                     print("PWD: %s" % os.environ['PWD'])
                     subprocess.call(command_line, shell=True)
                     n_submitted = n_submitted + 1
+
+    return n_submitted
+# end of function
+
+
+def process_single_network_folder(input_folder_path, output_folder_path, do_know_network_folder_path, network_folder_path_maybe, n_submitted) :
+    # print to show progress
+    print("%s:" % input_folder_path)
+
+    # get a list of all files and dirs in the source, dest dirs
+    try:
+        source_entries = os.listdir(input_folder_path)
+    except FileNotFoundError :
+        # if we can't list the dir, warn but continue
+        print("Warning: Folder %s doesn't seem to exist" % source_folder_path)   
+        return n_submitted
+    except PermissionError :
+        # if we can't list the dir, warn but continue
+        print("Warning: can't list contents of folder %s due to permissions error" % source_folder_path)
+        return n_submitted
+    
+    names_of_source_folders = [source_entry 
+                               for source_entry 
+                               in source_entries 
+                               if os.path.isdir(os.path.join(input_folder_path, source_entry))]
+    names_of_source_files = [source_entry 
+                             for source_entry 
+                             in source_entries 
+                             if os.path.isfile(os.path.join(input_folder_path, source_entry))]
+
+    if is_empty(network_folder_path_maybe) :
+        # This means that we're in the root of the single-network
+        # folder, so we look for a folder called "network", which
+        # should contain the trained network to use.
+        if "network" in names_of_source_folders :
+            network_folder_path = os.path.join(input_folder_path, "network")
+            network_folder_path_maybe = [ network_folder_path ]
+            names_of_source_folders = [ item for item in names_of_source_folders if item!="network" ]
+        else :
+            print("Unable to find network folder within %s" % input_folder_path)
+            return n_submitted
+    else:
+        # The maybe contains a value, so get it out This means that
+        # we're within a single-network folder, but not in the root of
+        # it.  Thus we already know the path to the trained network.
+        network_folder_path = network_folder_path_maybe[0]
+
+    # At this point, we know the network_folder_path, and
+    # names_of_source_folders doesn't contain it (which is good)
+
+    # Process the files in this folder
+    n_submitted = process_files_in_one_folder(input_folder_path, 
+                                              names_of_source_files, 
+                                              output_folder_path, 
+                                              network_folder_path, 
+                                              n_submitted)
         
-    # for each dir in names_of_folders_in_source_folder, recurse
+    # For each folder in names_of_source_folders, recurse
     for source_subfolder_name in names_of_folders_in_source_folder:
-        n_submitted = evaluate_on_folder(os.path.join(source_folder_path, source_subfolder_name),
-                                         os.path.join(output_folder_path, source_subfolder_name),
-                                         n_submitted)
+        n_submitted = process_single_network_folder(os.path.join(source_folder_path, source_subfolder_name),
+                                                    os.path.join(output_folder_path, source_subfolder_name),
+                                                    network_folder_path_maybe,
+                                                    n_submitted)
                     
     # return the updated count of copied files
     return n_submitted
+# end of function   
 
-# end of evaluate_on_folder
+
+def process_dilucid_root_folder(root_folder_path, root_output_folder_path):
+    # get a list of all files and dirs in the source, dest dirs
+    n_submitted = 0
+    try :
+        source_entries = os.listdir(root_folder_path)
+    except FileNotFoundError :
+        # if we can't list the dir, error
+        print("Warning: Root folder %s doesn't seem to exist" % root_folder_path)   
+        return n_submitted
+    except PermissionError :
+        # if we can't list the dir, error
+        print("Warning: can't list contents of root folder %s due to permissions error" % root_folder_path)
+        return n_submitted
+    
+    # separate source file, dir names
+    # names_of_folders_in_source_folder = []
+    # for source_entry in source_entries :
+    #     entry_path = os.path.join(root_folder_path, source_entry)
+    #     if os.path.isdir(entry_path) :
+    #         names_of_folders_in_source_folder.append(source_entry)            
+    names_of_folders_in_root_folder = [source_entry 
+                                       for source_entry 
+                                       in source_entries 
+                                       if os.path.isdir(os.path.join(root_folder_path, source_entry))]
+
+    # for each dir in names_of_folders_in_root_folder, run process_dilucid_network_folder
+    for network_folder_name in names_of_folders_in_root_folder:
+        n_submitted_this = process_single_network_folder(os.path.join(root_folder_path, network_folder_name),
+                                                         os.path.join(root_output_folder_path, network_folder_name),
+                                                         [],
+                                                         0)
+        n_submitted = n_submitted + n_submitted_this
+
+    return n_submitted
+# end of process_dilucid_root_folder
 
 
 
@@ -124,7 +195,7 @@ def evaluate_on_folder(source_folder_path, output_folder_path, n_submitted):
 # main
 source_root_folder_path = os.path.abspath(sys.argv[1])
 output_root_folder_path = os.path.abspath(sys.argv[2])
-n_submitted = evaluate_on_folder(source_root_folder_path, output_root_folder_path, 0)
+n_submitted = process_dilucid_root_folder(source_root_folder_path, output_root_folder_path)
 
 # print the number of jobs submitted
-print("%d jobs submitted" % n_submitted)
+print("%d jobs submitted total" % n_submitted)
